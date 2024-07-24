9e7ad299514e3576c43648a06b6b1584027deba5
Fernanda Dias <fernandadias.dev@gmail.com>
2024-07-24 15:37:40 -0300

    chore: Add Vite and Tailwind configuration files and TypeScript files

---
+++ frontend/src/app.tsx
@@ -0,0 +1,20 @@
+
+export function App() {
+
+  return (
+    <div className="h-screen flex items-center justify-center">
+      <div className="max-w-3xl px-6">
+        <p className="text-zinc-300 text-lg">Convide seus amigos e planeje sua próxima viagem!</p>
+
+        <div>
+          <input type="text" placeholder="Para onde você vai?"/>
+          <input type="text" placeholder="Quando?"/>
+        </div>
+
+        <p className="text-sm text-zinc-500">Ao planejar sua viagem pela plann.er você automaticamente concorda <br/>
+          com nossos <a className="text-zinc-500 underline" href="#">termos de uso</a> e <a className="text-zinc-500 underline" href="#">políticas de privacidade</a></p>
+      </div>
+      <P>PAREI EM 29:40!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!</P> 
+    </div>
+  )
+}
