1424edbca6cb451e8e97bb6aa69b7cc58b545e6d
Fernanda Dias <fernandadias.dev@gmail.com>
2024-07-30 12:58:34 -0300

    fix: Update lucide-react npm dependency

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -1,4 +1,4 @@
-import { MapPin, Calendar, ArrowRight, UserRoundPlus, Settings2, X, AtSign, } from 'lucide-react'
+import { MapPin, Calendar, ArrowRight, UserRoundPlus, Settings2, X, AtSign, Plus } from 'lucide-react'
 import { useState } from 'react'
 
 
@@ -122,12 +122,6 @@ export function App() {
               </div>
             </div>
 
-            {/* <div className="w-full h-px bg-zinc-800">
-              <form className=" p-2.5 bg-zinc-950 border border-zinc-800 rounded-lg">
-                <AtSign className="text-zinc-400 size-5" />
-              </form>
-            </div> */}
-
             <div className="w-full h-px bg-zinc-800" />
 
             <form
@@ -143,6 +137,11 @@ export function App() {
                   className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1"
                 />
               </div>
+              
+              <button className="bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gapt-2 hover:bg-lime-400">
+                Convidar
+                <Plus className="size-5" />
+              </button>
             </form>
 
           </div>
