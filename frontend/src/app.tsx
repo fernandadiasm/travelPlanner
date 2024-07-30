3d8110ef721d94509fea478c9f3b83cea1596bfb
Fernanda Dias <fernandadias.dev@gmail.com>
2024-07-30 09:31:52 -0300

    refactor: Update app layout and input styling

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -3,18 +3,19 @@ export function App() {
 
   return (
     <div className="h-screen flex items-center justify-center">
-      <div className="max-w-3xl px-6">
+      <div className="max-w-full px-6 text-center space-y-10">
         <p className="text-zinc-300 text-lg">Convide seus amigos e planeje sua próxima viagem!</p>
 
-        <div>
-          <input type="text" placeholder="Para onde você vai?"/>
-          <input type="text" placeholder="Quando?"/>
+        <div className="h-16 bg-zinc-900 px-4 rouded-xl flex items-center shadow-shape">
+          <input type="text" placeholder="Para onde você vai?" className="bg-transparent text-lg placeholder-zinc-400"/>
+          <input type="text" placeholder="Quando?" className="bg-transparent text-lg placeholder-zinc-400"/>
+
+          <button>Continuar</button>
         </div>
 
-        <p className="text-sm text-zinc-500">Ao planejar sua viagem pela plann.er você automaticamente concorda <br/>
+        <p className="text-sm text-zinc-500">Ao planejar sua viagem pela plann.er você automaticamente concorda <br />
           com nossos <a className="text-zinc-500 underline" href="#">termos de uso</a> e <a className="text-zinc-500 underline" href="#">políticas de privacidade</a></p>
       </div>
-      <P>PAREI EM 29:40!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!</P> 
     </div>
   )
 }
